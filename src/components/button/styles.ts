cad176f15eef39e7e7016a84630b5ebadd95c666
Xiaolin Jin Lin <dev@xiaojl.dev>
2024-12-01 19:40:03 +0100

    Test

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -6,7 +6,6 @@ export default css`
         color: white;
         padding: 15px 32px;
         text-align: center;
-        text-decoration: none;
         font-size: 16px;
         margin: 4px 2px;
         cursor: pointer;
