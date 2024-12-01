e201ef3c22a3dc18f2c7f69a7e58c249657ccb37
Xiaolin Jin Lin <dev@xiaojl.dev>
2024-12-01 19:37:18 +0100

    Test

--- src/components/button/styles.ts
+++ src/components/button/styles.ts
@@ -7,7 +7,6 @@ export default css`
         padding: 15px 32px;
         text-align: center;
         text-decoration: none;
-        display: inline-block;
         font-size: 16px;
         margin: 4px 2px;
         cursor: pointer;
